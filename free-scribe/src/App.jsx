c8504864b3b358f1c7643fe1d07d102997a249d4
Udith Supeshala <supeshalagunasekara@gmail.com>
2024-07-06 15:05:52 +0530

    Build functions and update current

--- free-scribe/src/App.jsx
+++ free-scribe/src/App.jsx
@@ -1,20 +1,49 @@
-import HomePage from "./components/HomePage"
-import Header from "./components/Header"
-
+import React, { useEffect, useState } from "react"; // Import React and useState hook
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
+import FileDisplay from "./components/FileDisplay";
+import Information from "./components/Information";
+import Transcribing from "./components/Transcribing";
 
 function App() {
- 
-  return (
-    <div className= 'flex flex-col  max-w-[1000px] max-auto w-full'>
-      <section className = 'min-h-screen flex flex-col'>
-        <Header></Header>
-        <HomePage></HomePage>
+  const [file, setFile] = useState(null);
+  const [audioStream, setAudioStream] = useState(null);
+  const [output, setOutput] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-        </section>
-     
-    
+  const isAudioAvailable = file || audioStream;
+
+  function handleAudiReset() {
+    setFile(null);
+    setAudioStream(null);
+    console.log("User click RESET buttton");
+  }
+
+  function printConsole() {
+    console.log("Here the function is working");
+  }
+
+  return (
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+
+        {output ? (
+          <Information />
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvailable ? (
+          <FileDisplay
+            handleAudiReset={handleAudiReset}
+            file={file}
+            audioStream={audioStream}></FileDisplay>
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+        )}
+      </section>
+      <footer></footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- free-scribe/src/components/Header.jsx
+++ free-scribe/src/components/Header.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <header className = 'p-4 flex items-center justify-between gap-4'>
-    <h1 className="font-medium"><span> Free <span className="text-blue-400 bold">Scribe</span></span></h1>
+    <header className=" p-4 flex items-center justify-between gap-4 ">
+      <a href="/">
+        <h1 className="font-medium text-2xl">
+          <span>
+            {/* {" "} */}
+            Free <span className=" text-blue-400 bold ">Scribe</span>
+          </span>
+        </h1>
+      </a>
 
-    <button className="flex items-center gap-2 specialBtn px-4 py-2 rounded-lg
-    text-blue-400">
-      <p>New</p>
-      <i className="fa-solid fa-plus"></i>
+      <a
+        href="/"
+        className=" flex items-center gap-2 specialBtn px-4 py-2 rounded-lg
+    text-blue-400 ">
+        <p>New</p>
 
-    </button>
-
-      
-    
-
-  </header>
-  )
+        <i className="fa-solid fa-plus"></i>
+      </a>
+    </header>
+  );
 }
